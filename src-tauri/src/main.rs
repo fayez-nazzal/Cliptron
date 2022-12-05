b94201b72cceba9aeaf65ade5b80d0a9ab6439b4
Fayez Nazzal <soulldev@gmail.com>
2022-12-05 22:39:53 +0200

    Move window to location

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,19 @@
     windows_subsystem = "windows"
 )]
 
+use enigo::Enigo;
+use enigo::MouseControllable;
+
+#[tauri::command]
+fn get_mouse_position() -> (i32, i32) {
+    let cursor_location: (i32, i32) = Enigo::new().mouse_location();
+
+    cursor_location
+}
+
 fn main() {
     tauri::Builder::default()
-        // .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![get_mouse_position])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,35 @@
 import { Close, Setting, Clear } from '@icon-park/react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { register } from '@tauri-apps/api/globalShortcut';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const onClose = () => {
+    const { appWindow } = require('@tauri-apps/api/window');
+
+    // don't close, just hide
+    appWindow.hide();
+  };
+
+  const onShortcut = async () => {
+    const mouse_position = await invoke("get_mouse_position") as [number, number];
+
+    const { appWindow, LogicalPosition } = require('@tauri-apps/api/window');
+
+    appWindow.setPosition(new LogicalPosition(mouse_position[0], mouse_position[1]));
+
+    appWindow.show();
+    appWindow.setAlwaysOnTop(true);
+  }
+
+  useEffect(() => {
+    register("CONTROL+X", onShortcut);
+  }, []);
+
   return (
     <div className="w-full h-8">
       <div data-tauri-drag-region className="flex flex-row-reverse p-2 border-b-2" id="titlebar-close">
-        <button className='text-[#444] hover:text-red-500'>
+        <button className='text-[#444] hover:text-red-500' onClick={onClose}>
           <Close theme="outline" size="18" fill="currentColor" />
         </button>
 
