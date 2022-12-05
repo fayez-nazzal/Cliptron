6c14127f5b1f084b95391cef4bf413cdbcca8af4
Fayez Nazzal <soulldev@gmail.com>
2022-12-05 21:50:19 +0200

    Add main title-bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import { Close, Setting, Clear } from '@icon-park/react';
+
 function App() {
   return (
-    <div>
+    <div className="w-full h-8">
+      <div data-tauri-drag-region className="flex flex-row-reverse p-2 border-b-2" id="titlebar-close">
+        <button className='text-[#444] hover:text-red-500'>
+          <Close theme="outline" size="18" fill="currentColor" />
+        </button>
+
+        <button className='mr-1 text-[#444] hover:text-blue-500'>
+          <Setting theme="outline" size="18" fill="currentColor" />
+        </button>
+
+        <div className="mr-auto text-sm">
+          200 copied items
+        </div>
+
+        <button className='mr-1 text-[#444] hover:text-green-500'>
+          <Clear theme="outline" size="18" fill="currentColor" />
+        </button>
+      </div>
     </div>
   );
 }
