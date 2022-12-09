3408a3542231e11c65f2b7dfb751ed8652c4b534
Fayez Nazzal <49946791+fayez-nazzal@users.noreply.github.com>
2022-12-09 18:43:23 +0200

    Make setting entries equal in size

--- src/components/SettingsEntry/index.tsx
+++ src/components/SettingsEntry/index.tsx
@@ -2,7 +2,7 @@ import { ISettingsEntryProps } from "./index.types";
 
 export const SettingsEntry = ({ label, children }: ISettingsEntryProps) => {
   return (
-    <div className="flex w-full justify-between items-center p-2 px-3 bg-gray-100 rounded-lg">
+    <div className="flex w-full justify-between items-center p-2 px-3 bg-gray-100 rounded-lg min-h-16 h-16 max-h-16">
       {label && <span>{label}</span>}
       {children}
     </div>
