af94d5dae6562f6097d6290f4bf855b0db9bc70f
Fayez Nazzal <49946791+fayez-nazzal@users.noreply.github.com>
2022-12-09 18:10:46 +0200

    Functional OK button on settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -6,11 +6,19 @@ import { TextInput } from "../components/Input/index";
 import { MaxItemsInput } from "@components/MaxItemsInput/index";
 import { ShortcutInput } from "../components/ShortcutInput/index";
 import { ShortcutSettingsEntry } from "../components/ShortcutSettingsEntry/index";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const SettingsPage = () => {
+  const router = useRouter();
+
+  const onOK = () => {
+    router.push("/");
+  };
+
   return (
     <div className="w-screen h-screen gap-2 flex flex-col text-center items-center px-6">
-      <h1 className="text-2xl mt-4 mb-2">Settings</h1>
+      <h1 className="text-2xl mt-4 mb-1">Settings</h1>
 
       <SettingsEntry label="App Theme">
         <ThemeToggle />
@@ -26,13 +34,12 @@ const SettingsPage = () => {
 
       <ShortcutSettingsEntry />
 
-      <div className="flex w-full justify-center mt-auto mb-20  gap-2">
-        <button className="bg-gray-500 text-white rounded-lg p-2">
-          Cancel
-        </button>
-
-        <button className="bg-blue-500-light dark:bg-blue-500-dark text-white rounded-lg p-2 flex-[0.4]">
-          Save
+      <div className="flex w-full justify-center mt-auto mb-16 gap-2">
+        <button
+          className="bg-blue-500-light dark:bg-blue-500-dark text-white rounded-lg p-2 flex-[0.4] hover:brightness-95 active:ring"
+          onClick={onOK}
+        >
+          OK
         </button>
       </div>
     </div>
