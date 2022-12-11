6041710a96f48e3ea2e5e072b6ff4e5327a4f1cc
Fayez Nazzal <soulldev@gmail.com>
2022-12-11 06:26:31 +0200

    Exchange hiding countdown with a simple timer

--- src/pages/hiding.tsx
+++ src/pages/hiding.tsx
@@ -1,33 +1,24 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { hide_window, on_shortcut } from '../actions/tauri';
+import { useEffect } from "react";
+import { hide_window } from '../actions/tauri';
 
 const hidingPage = () => {
-  const [countdown, setCountdown] = useState(5);
   const router = useRouter();
 
-  const decreaseCountdown = () => {
-    setCountdown(countdown - 1);
-  };
-
-  const setCountdownTimer = () => {
-    setTimeout(() => {
-      decreaseCountdown();
-    }, 1000);
-  };
-
-  const onCountdownEnd = () => {
+  const onHide = () => {
     hide_window();
     router.push("/");
   };
 
+  const setHideTimeout = () => {
+    setTimeout(() => {
+      onHide();
+    }, 2000);
+  };
+
   useEffect(() => {
-    if (countdown > 0) {
-      setCountdownTimer();
-    } else {
-      onCountdownEnd();
-    }
-  }, [countdown]);
+    setHideTimeout();
+  }, []);
 
   return (
     <div className="w-screen h-full gap-2 flex flex-col text-center items-center px-6">
@@ -36,7 +27,6 @@ const hidingPage = () => {
       <p className="text-lg mt-2">
         You can always show it again by pressing the shortcut.
       </p>
-      <p className="text-5xl mt-3 mb-1 text-blue-500-dark">{countdown}</p>
     </div>
   );
 };
